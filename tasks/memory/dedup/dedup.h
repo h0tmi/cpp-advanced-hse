cd637aa216e278f9dec487259bf2cae86c096e1f
Timofey Zhamoydin <timofeyzhamoidin@gmail.com>
2023-08-07 20:04:05 +0300

    Solved dedup

--- tasks/memory/dedup/dedup.h
+++ tasks/memory/dedup/dedup.h
@@ -3,17 +3,29 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using std::string;
 
 std::vector<std::unique_ptr<string>> Duplicate(const std::vector<std::shared_ptr<string>>& items) {
-    // Your code here
+    std::vector<std::unique_ptr<string>> result;
 
-    return {};
+    for (const auto& i : items) {
+        result.emplace_back(std::make_unique<string>(i->data()));
+    }
+
+    return result;
 }
 
 std::vector<std::shared_ptr<string>> DeDuplicate(const std::vector<std::unique_ptr<string>>& items) {
-    // Your code here
+    std::unordered_map<string, std::shared_ptr<string>> checker;
+    std::vector<std::shared_ptr<string>> result;
 
-    return {};
+    for (const auto& i : items) {
+        const auto [it, _] = checker.try_emplace(i->data(), std::make_shared<string>(i->data()));
+
+        result.emplace_back(it->second);
+    }
+
+    return result;
 }
